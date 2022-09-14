840a00cd5f31651fb325bb1d660167fee453ab02
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-14 17:44:04 +0530

    first name validating with ms test

---
+++ UserRegistrationTestExceptionLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationTestExceptionLambda
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MSTest REGEX User Registration Program");
+        }
+    }
+}
---
+++ UserRegistrationTestExceptionLambda/REGEX_Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationTestExceptionLambda
+{
+    public class REGEX_Registration
+    {
+        public static string First_Name = "^[A-Z]{1}[a-z]{2,10}$";
+
+        public bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, First_Name);
+
+        }
+    }
+}
