0971a994501d8b57f6fbc7e9341eba4a0f13439d
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-14 17:46:45 +0530

    first name validating with ms test

---
+++ MSTest_Registration/REGEX_Registration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSTest_Registration
+{
+    internal class REGEX_Registration
+    {
+        internal bool ValidateFirstName(string firstName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MSTest_Registration/UnitTest1.cs
+++ MSTest_Registration/UnitTest1.cs
@@ -5,9 +5,24 @@ namespace MSTest_Registration
     [TestClass]
     public class UnitTest1
     {
+
+        //UC1
+
         [TestMethod]
-        public void TestMethod1()
+        public void FirstName()
         {
+            //Arrange
+
+            REGEX_Registration obj = new REGEX_Registration();
+
+            bool expected = true;
+            string FirstName = "Hemant";
+
+            //Act
+            bool var = obj.ValidateFirstName(FirstName);
+
+            //Assert
+            Assert.AreEqual(expected, var);
         }
     }
 }
